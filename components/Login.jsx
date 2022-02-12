9f4f15f8706d3a0b440120a2aca0be8fb2d6182e
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2022-02-12 23:01:43 +0530

    feat: login flow with moralis complete

---
+++ components/Login.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import { useMoralis } from "react-moralis";
+
+import BG from '../public/meta-bg.png'
+
+const Login = () => {
+    const { authenticate, isInitializing } = useMoralis();
+
+    return (
+        <div className="h-screen w-full bg-black relative grid items-center justify-center text-white">
+
+            <div className="flex items-center justify-center z-10">
+                <div className="relative group">
+                    <div
+                        className="absolute -inset-0.5 bg-white group-hover:bg-gradient-to-r group-hover:from-pink-600 group-hover:to-purple-600 rounded-md blur opacity-75 group-hover:opacity-100 transition duration-1000"></div>
+                    <button onClick={authenticate}
+                        className="relative bg-white text-red-600 font-bold  py-3 px-6 rounded-md">
+                        Login to the Web 3.0 Chat
+                    </button>
+                </div>
+            </div>
+
+            <Image src={BG} layout='fill' objectFit="cover" />
+        </div>
+    )
+}
+
+export default Login
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Inter', 'system-ui'],
+      'serif': ['Inter', 'system-ui'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
